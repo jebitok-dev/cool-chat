f40ab6d9363a6cb9d281681f5ae24658f3667071
jebitok-dev <sharonkosgei4@gmail.com>
2020-08-11 03:08:17 +0300

    Updates

---
+++ precache-manifest.56c5514e1d90646a817060be24b2ea7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "520ca59cf95d9ff62e5b6ccc3cf8d1df",
+    "url": "/jebitok-dev/chat-app/index.html"
+  },
+  {
+    "revision": "b003135b53bd8aa3d9f2",
+    "url": "/jebitok-dev/chat-app/static/css/main.f13ce8d4.chunk.css"
+  },
+  {
+    "revision": "f96399426e8daff006fb",
+    "url": "/jebitok-dev/chat-app/static/js/2.8c64c302.chunk.js"
+  },
+  {
+    "revision": "b003135b53bd8aa3d9f2",
+    "url": "/jebitok-dev/chat-app/static/js/main.80837395.chunk.js"
+  },
+  {
+    "revision": "c295636664a30650cc46",
+    "url": "/jebitok-dev/chat-app/static/js/runtime~main.061aacea.js"
+  }
+]);
